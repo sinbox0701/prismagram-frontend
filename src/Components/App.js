54296b855faec140a40f88c6b9587690f981b167
sinbox0701 <psh090953@gmail.com>
2020-10-29 16:23:52 +0900

    Header UI

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,15 @@ import React from "react";
 //Components 대신 React-Hook
 import {gql} from "apollo-boost";
 import styled,{ThemeProvider} from "styled-components";
+import {HashRouter as Router} from "react-router-dom";
 import { useQuery } from "react-apollo-hooks";
 import {ToastContainer, toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
-import AppRouter from "./Router";
+import Routes from "./Router";
 import Footer from "./Footer";
+import Header from "./Header";
 
 const QUERY = gql`
   {
@@ -18,7 +20,7 @@ const QUERY = gql`
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 935px;
+  max-width: ${props => props.theme.maxWidth};
   width: 100%;
 `;
 
@@ -29,12 +31,19 @@ export default () => {
 
   return (  
    <ThemeProvider theme={Theme}>
-    <Wrapper>
-      <GlobalStyles/>
-      <AppRouter isLoggedIn={isLoggedIn}/>
-      <Footer/>
-      <ToastContainer position={toast.POSITION.BOTTOM_LEFT}/>
-    </Wrapper>
+     <>
+        <GlobalStyles/>
+        <Router>
+          <>
+            <Header/>
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn}/>
+              <Footer/>
+            </Wrapper>
+          </>    
+        </Router>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT}/>
+      </>
   </ThemeProvider>
   );
 };
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,7 +1,7 @@
 //Routes directory에는 page별 js파일이 들어있고 이를 불러오는것이 Router.js
 import React from "react";
 import PropTypes from "prop-types";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 
@@ -20,9 +20,7 @@ const LoggedOutRoutes = () => (
 
 //Router has only one prob => isLoggedIn
 const AppRouter = ({isLoggedIn}) => (
-    <Router>
-        <Switch>{isLoggedIn ? <LoggedInRoutes/>:<LoggedOutRoutes/>}</Switch>
-    </Router>
+    <Switch>{isLoggedIn ? <LoggedInRoutes/>:<LoggedOutRoutes/>}</Switch>
 );
 
 AppRouter.propTypes = {
