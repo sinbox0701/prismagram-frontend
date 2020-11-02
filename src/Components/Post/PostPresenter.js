2bc7c11cb0d90642a5af27dc11705c7e214ce26e
sinbox0701 <psh090953@gmail.com>
2020-11-02 16:29:21 +0900

    Profile

--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -4,6 +4,7 @@ import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment as CommentIcon } from "../Icons";
+import { Link } from "react-router-dom";
 
 const Post = styled.div`
   ${props => props.theme.whiteBox};
@@ -11,6 +12,9 @@ const Post = styled.div`
   max-width: 600px;
   margin-bottom: 25px;
   user-select: none;
+  a{
+    color: inherit;
+  }
 `;
 
 const Header = styled.header`
@@ -101,6 +105,10 @@ const Comment = styled.li`
   }
 `;
 
+const Caption = styled.div`
+  margin: 10px 0px;
+`;
+
 export default ({
   user: { username, avatar },
   location,
@@ -113,13 +121,16 @@ export default ({
   toggleLike,
   onKeyPress,
   comments,
-  selfComments
+  selfComments,
+  caption
 }) => (
   <Post>
     <Header>
       <Avatar size="sm" url={avatar} />
       <UserColumn>
-        <FatText text={username} />
+        <Link to={`/${username}`}>
+          <FatText text={username} />
+        </Link>
         <Location>{location}</Location>
       </UserColumn>
     </Header>
@@ -137,6 +148,9 @@ export default ({
         </Button>
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+      <Caption>
+        <FatText text={username} /> {caption}
+      </Caption>
       {comments && (
         <Comments>
           {comments.map(comment => (
--- src/Routes/Profile.js
+++
@@ -1,2 +0,0 @@
-export default () => "Profile";
-//page 확인용 source code
